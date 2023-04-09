401d77f9b3af2427f51bafb10cda4582fd94cf6c
Sand82 <sandoki@abv.bg>
2023-04-09 23:37:23 +0300

    added new exercise

---
+++ Data Structures Fundamentals-C#/MyLinkedList/MyLinkedList/MyLinkedList/MyLinkedList.cs
@@ -0,0 +1,62 @@
+﻿namespace MyLinkedList
+{
+    public class MyLinkedList<T>
+    {
+        public Node<T>? Head { get; set; }
+        public Node<T>? Last { get; set; }       
+
+        public void Add(T value)
+        {
+            var head = new Node<T>(value);
+
+            if (Head == null)
+            {
+                Head = head;
+                Last = head;
+            }
+            else
+            {
+                head.Next = Head;
+                Head = head;
+            }            
+        }
+
+        public void AddLast(T value)
+        {
+            var last = new Node<T>(value);
+
+            if (Last == null)
+            {
+                Head = last;
+                Last = last;
+            }
+            else
+            {
+                Last.Next = last;
+                Last = last;
+            }
+        }
+
+        public Node<T>? Remove()
+        {
+            if (Head == null)
+            {
+                return null;
+            }
+
+            var removedNode = Head;
+
+            if (Head.Next == null)
+            {
+                Last = null;
+                Head = null;
+            }
+            else
+            {
+                Head = Head.Next;
+            }
+
+            return removedNode;
+        }       
+    }
+}
---
+++ Data Structures Fundamentals-C#/MyLinkedList/MyLinkedList/MyLinkedList/Program.cs
@@ -0,0 +1,22 @@
+﻿using MyLinkedList;
+
+var linkedList = new MyLinkedList<int>();
+
+for (int i = 1; i <= 10; i++)
+{
+    linkedList.Add(i);
+}
+
+//linkedList.Remove();
+//linkedList.Remove();
+//linkedList.Remove();
+//linkedList.Remove();
+
+var currNode = linkedList.Head;
+
+while (currNode != null)
+{
+    Console.WriteLine(currNode.Value);
+
+    currNode = currNode.Next;
+}
