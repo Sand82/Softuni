12e8f22db1003105ee2ab067d4d5267569832d80
Sand82 <sandoki@abv.bg>
2023-04-15 22:44:56 +0300

    added new exercise

---
+++ Data Structures Fundamentals-C#/BinarySearchTree(BST)/BinarySearchTree/BinarySearchTree/BST.cs
@@ -0,0 +1,84 @@
+﻿namespace BinarySearchTree
+{
+    public class BST<T> where T : IComparable<T>
+    {
+        public BST(Node<T> root = null)
+        {
+            this.Root = root;
+        }
+
+        public Node<T> Root { get; set; }
+
+        public void Insert(T value, Node<T> node)
+        {
+            if (node == null)
+            {
+                Root = new Node<T>(value);
+                return;
+            }
+
+            if (node.Value.CompareTo(value) > 0)
+            {
+                if (node.LeftChild == null)
+                {
+                    node.LeftChild = new Node<T>(value);
+                    return;
+                }
+
+                Insert(value, node.LeftChild);
+            }
+            else
+            {
+                if (node.RightChild == null)
+                {
+                    node.RightChild = new Node<T>(value);
+                    return;
+                }
+
+                Insert(value, node.RightChild);
+            }
+        }
+
+        public bool Contains(T value, Node<T> node)
+        {
+            if (node == null)
+            {                
+                return false;
+            }
+
+            if (node.Value.CompareTo(value) == 0)
+            {
+                return true;
+            }
+
+            if (node.Value.CompareTo(value) > 0)
+            {
+                return Contains(value, node.LeftChild);
+            }
+            else
+            {
+                return Contains(value, node.RightChild);
+            }
+        }
+
+        public string DFSInOrder(Node<T> root, int indent)
+        {
+            string result = "";
+
+            if (root.LeftChild != null)
+            {
+                result += DFSInOrder(root.LeftChild, indent + 3);
+            }
+
+            result += $"{ new string(' ', indent)}{root.Value}\n";
+
+            if (root.RightChild != null)
+            {
+                result += DFSInOrder(root.RightChild, indent + 3);
+            }
+
+            return result;
+        }
+    }
+
+}
---
+++ Data Structures Fundamentals-C#/BinarySearchTree(BST)/BinarySearchTree/BinarySearchTree/Program.cs
@@ -0,0 +1,31 @@
+﻿using BinarySearchTree;
+
+var list = new List<int>();
+
+for (int i = 0; i < 50; i += 2)
+{
+    list.Add(i);
+}
+
+BST<int> tree = new BST<int>();
+
+Insert(tree, 0, list.Count, list);
+
+Console.WriteLine(tree.DFSInOrder(tree.Root, 0));
+Console.WriteLine(tree.Contains(40, tree.Root));
+Console.WriteLine(tree.Contains(100, tree.Root));
+
+
+
+void Insert(BST<int> tree, int start, int end, List<int> list)
+{
+    if (start >= end)
+    {
+        return;
+    }
+
+    var middle = (start + end) / 2;
+    tree.Insert(list[middle], tree.Root);
+    Insert(tree, start, middle - 1, list);
+    Insert(tree, middle + 1, end, list);
+}
