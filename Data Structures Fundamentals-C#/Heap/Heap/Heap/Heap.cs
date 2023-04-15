fce7f3ec64f089e544c9cbd30667c77095bb9ac3
Sand82 <sandoki@abv.bg>
2023-04-15 21:01:45 +0300

    added new exercise

---
+++ Data Structures Fundamentals-C#/Heap/Heap/Heap/Heap.cs
@@ -0,0 +1,65 @@
+﻿namespace Heap
+{
+    public class Heap<T> where T : IComparable<T>
+    {
+        private List<T> heap;
+
+        public Heap()
+        {
+          heap = new List<T>();
+        }
+        public int Size { get { return heap.Count; } }
+
+        public T GetMax()
+        {
+            return heap[0];
+        }
+
+        public string PringHeap(int index, int indent)
+        {
+            var result = "";
+
+            var leftChild = (index * 2) + 1;
+            var rightChild = (index * 2) + 2;
+
+            if (leftChild < heap.Count)
+            {
+                result += PringHeap(leftChild, indent + 3);
+            }
+
+            result += $"{new string(' ', indent)}{heap[index]}\r\n";
+
+            if (rightChild < heap.Count)
+            {
+                result += PringHeap(rightChild, indent + 3);
+            }
+
+            return result;
+        }
+
+        public void Add(T element)
+        {
+            heap.Add(element);
+            int index = heap.Count - 1;
+            Heapify(index);
+        }
+
+        private void Heapify(int index)
+        {
+            if (index == 0)
+            {
+                return;
+            }
+
+            var parentIndex = (index - 1) / 2;            
+
+            if (heap[index].CompareTo(heap[parentIndex]) > 0)
+            {
+                var temp = heap[index];
+                heap[index] = heap[parentIndex];
+                heap[parentIndex] = temp;
+                Heapify(parentIndex);
+            }
+        }
+    }
+}
---
+++ Data Structures Fundamentals-C#/Heap/Heap/Heap/Program.cs
@@ -0,0 +1,15 @@
+﻿using Heap;
+
+var heap = new Heap<int>();
+
+var array = new List<int>() { 15, 6, 9, 5, 25, 8, 17, 16 };
+
+for (int i = 0; i < array.Count; i++)
+{
+    heap.Add(array[i]);
+}
+
+heap.Add(100);
+heap.Add(7);
+
+Console.WriteLine(heap.PringHeap(0, 0));
