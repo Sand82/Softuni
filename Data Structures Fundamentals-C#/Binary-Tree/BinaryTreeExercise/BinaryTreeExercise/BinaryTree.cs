63481b96675095b76edc59e70df23b4eb1f54fe7
Sand82 <sandoki@abv.bg>
2023-04-15 19:46:25 +0300

    added new exercise

---
+++ Data Structures Fundamentals-C#/Binary-Tree/BinaryTreeExercise/BinaryTreeExercise/BinaryTree.cs
@@ -0,0 +1,67 @@
+﻿
+namespace BinaryTreeExercise
+{
+    public class BinaryTree<T>
+    {
+        public BinaryTree(Node<T> root)
+        {
+            this.Root = root;
+        }
+        public Node<T> Root { get; set; }
+
+        public string DFSPreOrder(Node<T> root, int indent)
+        {
+            var result = $"{ new string(' ', indent)}{root.Value}\r\n";
+
+            if (root.LeftChild != null)
+            {
+                result += DFSPreOrder(root.LeftChild, indent + 3);
+            }
+
+            if (root.RightChild != null)
+            {
+                result += DFSPreOrder(root.RightChild,indent + 3);
+            }
+
+            return result;
+        }
+
+        public string DFSInOrder(Node<T> root, int indent)
+        {
+            string result = "";
+
+            if (root.LeftChild != null)
+            {
+                result += DFSInOrder(root.LeftChild, indent + 3);
+            }
+
+            result += $"{ new string(' ', indent)}{root.Value}\n";
+
+            if (root.RightChild != null)
+            {
+                result += DFSInOrder(root.RightChild, indent + 3);
+            }
+
+            return result;
+        }
+
+        public string DFSPostOrder(Node<T> root, int indent)
+        {
+            string result = "";
+
+            if (root.LeftChild != null)
+            {
+                result += DFSPostOrder(root.LeftChild, indent + 3);
+            }
+
+            if (root.RightChild != null)
+            {
+                result += DFSPostOrder(root.RightChild, indent + 3);
+            }
+
+            result += $"{ new string(' ', indent)}{root.Value}\n";
+
+            return result;
+        }
+    }
+}
---
+++ Data Structures Fundamentals-C#/Binary-Tree/BinaryTreeExercise/BinaryTreeExercise/Program.cs
@@ -0,0 +1,11 @@
+﻿using BinaryTreeExercise;
+
+var root = new Node<int>(1,
+    new Node<int>(5, new Node<int>(2), new Node<int>(3)),
+    new Node<int>(7, new Node<int>(9), new Node<int>(11))
+    );
+
+var binaryTree = new BinaryTree<int>(root);
+
+Console.WriteLine(binaryTree.DFSInOrder(root, 0));
+
