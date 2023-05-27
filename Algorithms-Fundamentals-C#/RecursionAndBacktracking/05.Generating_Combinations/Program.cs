40174aa255be272bcd988a5832d036b0418dabaf
Sand82 <sandoki@abv.bg>
2023-05-28 01:00:00 +0300

    added new exercise

---
+++ Algorithms-Fundamentals-C#/RecursionAndBacktracking/05.Generating_Combinations/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace GeneratingCombinations
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+            int n = int.Parse(Console.ReadLine());
+
+            int index = 0;
+            int border = 0;
+
+            var resultArr = new int[n];
+
+            GeneratingCombinations(arr, resultArr, index, border);
+        }
+
+        private static void GeneratingCombinations(int[] set, int[] vector, int index, int border)
+        {
+            if (vector.Length == index)
+            {
+                Console.WriteLine(string.Join(" ", vector));
+                return;
+            }
+
+            for (int i = border; i < set.Length; i++)
+            {
+                vector[index] = set[i];
+
+                GeneratingCombinations(set, vector, index + 1, i + 1);
+            }
+        }
+    }
+}
---
+++ Algorithms-Fundamentals-C#/RecursionAndBacktracking/07.Paths_In_Labyrinth/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+namespace PathsInLabyrinth
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var row = int.Parse(Console.ReadLine());
+
+            var col = int.Parse(Console.ReadLine());
+
+            char[,] martix = new char[row, col];
+
+            for (int i = 0; i < row; i++)
+            {
+                var input = Console.ReadLine();
+
+                for (int j = 0; j < col; j++)
+                {
+                    martix[i, j] = input[j];
+                }
+            }
+
+            int startRow = 0;
+            int startCol = 0;
+            string direction = "";
+            var directionResult = new List<string>();
+
+            FindPats(martix, startRow, startCol, direction, directionResult);
+        }
+
+        private static void FindPats(char[,] matrix, int row, int col, string direction, List<string> directionResult)
+        {
+            if (row < 0 || row >= matrix.GetLength(0) || col < 0 || col >= matrix.GetLength(1))
+            {
+                return;
+            }
+
+            if (matrix[row, col] == '*'|| matrix[row, col] == 'v')
+            {
+                return;
+            }
+
+            directionResult.Add(direction);
+
+            if (matrix[row, col] == 'e')
+            {
+               
+                Console.WriteLine(string.Join("", directionResult));
+                directionResult.RemoveAt(directionResult.Count - 1);
+                return;
+            }
+
+            matrix[row, col] = 'v';           
+
+            FindPats(matrix, row - 1, col, "U", directionResult);  //Up
+            FindPats(matrix, row + 1, col, "D", directionResult);  //Down
+            FindPats(matrix, row, col - 1, "L", directionResult);  //Left
+            FindPats(matrix, row, col + 1, "R", directionResult);  //Up
+
+            matrix[row, col] = '-';
+            directionResult.RemoveAt(directionResult.Count - 1);
+        }
+    }
+}
