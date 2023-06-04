fd9e488071e7cff261255646cf150ed0d1769334
Sand82 <sandoki@abv.bg>
2023-06-04 23:07:09 +0300

    added new exercise

---
+++ Algorithms-Fundamentals-C#/Combinatorial/PermutationsWithoutRepetition/NChooseKCount/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace NChooseKCount
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n =int.Parse(Console.ReadLine());
+
+            int k =int.Parse(Console.ReadLine());
+
+            Console.WriteLine(GetBinom(n, k));
+        }
+
+        private static int GetBinom(int row, int col)
+        {
+            if (row <= 0 || col == 0 || row == col)
+            {
+                return 1;
+            }
+
+            return GetBinom(row - 1, col) + GetBinom(row - 1, col - 1);
+        }
+    }
+}
