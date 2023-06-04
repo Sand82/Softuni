d2a0c5e466361635e68d18bd09b2612ef176abd0
Sand82 <sandoki@abv.bg>
2023-06-04 22:27:31 +0300

    added new exercise

---
+++ Algorithms-Fundamentals-C#/Combinatorial/PermutationsWithoutRepetition/CombinationsWithoutRepetition/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CombinationsWithoutRepetitio
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ Algorithms-Fundamentals-C#/Combinatorial/PermutationsWithoutRepetition/VariationsWithRepetition/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace VariationsWithRepetition
+{
+    public class Program
+    {
+        private static int k;
+        private static string[] elements;
+        private static string[] variations;
+
+        private static int count= 0;
+
+        public static void Main(string[] args)
+        {
+            elements = Console.ReadLine().Split(' ');
+
+            k = int.Parse(Console.ReadLine());
+
+            variations = new string[k];
+
+            Variations(0);
+
+            Console.WriteLine($"Count of combinations {count}");
+        }
+
+        private static void Variations(int index)
+        {
+            if (index >= variations.Length)
+            {
+                count++;
+                Console.WriteLine(String.Join(" ", variations));
+                return;
+            }
+
+            for (int i = 0; i < elements.Length; i++)
+            {
+                variations[index] = elements[i];
+                Variations(index + 1);
+            }
+        }
+    }
+}
