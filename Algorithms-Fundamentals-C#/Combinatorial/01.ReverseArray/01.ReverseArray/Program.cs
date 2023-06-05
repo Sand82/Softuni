2076cb138229d9250d83ce359b84b451c11267c3
Sand82 <sandoki@abv.bg>
2023-06-05 10:45:21 +0300

    added new exercise

---
+++ Algorithms-Fundamentals-C#/Combinatorial/01.ReverseArray/01.ReverseArray/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace ReverseArray
+{
+    public class Program
+    {
+        private static int[] arr;
+
+        private static int[] result;
+
+        private static int counter = 0;
+
+        public static void Main(string[] args)
+        {
+            arr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            result = new int[arr.Length];
+
+            var index = arr.Length - 1;
+
+            ReverseArray(index);
+        }
+
+        private static void ReverseArray(int index)
+        {
+            if (index == 0)
+            {
+                result[counter] = arr[index];                
+                Console.WriteLine(String.Join(" ", result));
+                return;
+            }
+
+            result[counter] = arr[index];
+            counter++;
+            ReverseArray(index - 1);
+        }
+    }
+}
+
---
+++ Algorithms-Fundamentals-C#/Combinatorial/01.ReverseArray/01.ReverseArraySwopElements/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ReverseArraySwopElements
+{
+    public class Program
+    {
+        private static int[] arr;
+        public static void Main(string[] args)
+        {
+            arr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            ReverceArr(0);
+        }
+
+        private static void ReverceArr(int index)
+        {
+            if (index == arr.Length / 2)
+            {
+                return;
+            }
+
+            var temp  = arr[index];
+            arr[index] = arr[arr.Length - index - 1];
+            arr[arr.Length - index - 1] = temp;
+            
+            ReverceArr(index + 1);
+        }
+    }
+}
