d323ae0216a36917eee29fc4a513a3b4b00ed16e
Sand82 <sandoki@abv.bg>
2023-06-05 12:38:37 +0300

    added new exercise

---
+++ Algorithms-Fundamentals-C#/ExerciseRecursionAndCombinatorialAlgorithms/02.NestedLoops/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace NestedLoops
+{ 
+    public class Program
+    {
+        private static int[] result;
+        
+        private static int n;        
+
+        public static void Main(string[] args)
+        {
+            n = int.Parse(Console.ReadLine());           
+
+            result = new int[n];
+
+            NestedLoops(0);
+        }
+
+        private static void NestedLoops(int index)
+        {
+            if (index >= result.Length)
+            {                
+                Console.WriteLine(String.Join(" ", result));
+                return;
+            }
+
+            for (int i = 1; i <= n; i++)
+            {
+                result[index] = i;
+                NestedLoops(index + 1);
+            }
+        }
+    }
+}
