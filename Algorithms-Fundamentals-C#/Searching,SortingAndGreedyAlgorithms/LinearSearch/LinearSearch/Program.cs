5d5f920c945257e35651caecfb64f8d4cb13206f
Sand82 <sandoki@abv.bg>
2023-06-14 21:59:43 +0300

    added new exercise

---
+++ Algorithms-Fundamentals-C#/Searching,SortingAndGreedyAlgorithms/LinearSearch/LinearSearch/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace LinerSearch
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var arr = new List<int>() { 4, 5, 3, 2, 1 };
+
+            var element = 22;
+
+            int result = LenearSearch(arr, element);
+
+            Console.WriteLine(result);
+        }
+
+        private static int LenearSearch(List<int> arr, int element)
+        {
+            for (int i = 0; i < arr.Count; i++)
+            {
+                if (arr[i] == element)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
