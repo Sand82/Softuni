f1b80619d19380dac659433873088f6fc2be6cb8
Sand82 <sandoki@abv.bg>
2023-06-15 08:19:43 +0300

    added new exercise

---
+++ Algorithms-Fundamentals-C#/Searching,SortingAndGreedyAlgorithms/LinearSearch/SelectionSort/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace SelectionSort
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var arr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            SelectionSort(arr);
+
+            Console.WriteLine(String.Join(" ", arr));
+        }
+
+        private static void SelectionSort(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                var index = i;
+
+                for (int j = 0; j < arr.Length; j++)
+                {
+                    if (arr[j] < arr[i])
+                    {
+                        index = j;
+                    }
+                }
+
+                Swap(arr, i, index);
+            }
+        }
+
+        private static void Swap(int[] arr, int firstElement, int secondElement)
+        {
+            var temp = arr[firstElement];
+            arr[firstElement] = arr[secondElement];
+            arr[secondElement] = temp;
+        }
+    }
+}
