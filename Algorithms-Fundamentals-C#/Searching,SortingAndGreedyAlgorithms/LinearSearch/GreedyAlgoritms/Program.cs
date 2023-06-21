d561d695ab3f8de9871d94223cdd1b0a0680c12e
Sand82 <sandoki@abv.bg>
2023-06-21 22:05:35 +0300

    added new exercise

---
+++ Algorithms-Fundamentals-C#/Searching,SortingAndGreedyAlgorithms/LinearSearch/GreedyAlgoritms/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace GreedyAlgoritms
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var coints = new Queue<int>(Console.ReadLine().Split(", ").Select(int.Parse).OrderByDescending(x => x));
+
+            var targetSum = int.Parse(Console.ReadLine());
+
+            Dictionary<int, int> countOfCoints = new Dictionary<int, int>();
+
+            var usedCointsCount = 0;
+
+            while (targetSum > 0 && coints.Count > 0)
+            {
+               var coint = coints.Dequeue();
+
+                var count = targetSum / coint;
+
+                targetSum %= coint;
+
+                if (count == 0)
+                {
+                    continue;
+                }
+
+                usedCointsCount += count;
+
+                countOfCoints.Add(coint, count);
+            }
+
+            if (targetSum > 0)
+            {
+                Console.WriteLine("Error ");
+            }
+            else
+            {
+                Console.WriteLine($"Number of coins to take: {usedCointsCount}");
+
+                foreach (var coint in countOfCoints)
+                {
+                    Console.WriteLine($"{coint.Key} coin(s) with value {coint.Value}");
+                }
+            }
+        }
+    }
+}
