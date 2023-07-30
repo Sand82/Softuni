e9da1ea8cedfa469c9c5ade352c38af44eae32d1
Sand82 <sandoki@abv.bg>
2023-07-30 18:28:55 +0300

    added new exercise

---
+++ Algorithms-Fundamentals-C#/IntroductionToDynamicProgramming/01.Fibonacci/01.Fibonacci/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Fibonacci
+{
+    public class Program
+    {
+        private static Dictionary<int, long> cache = new Dictionary<int, long>();
+
+        public static void Main(string[] args)
+        {
+           int n = int.Parse(Console.ReadLine());
+
+            var result = CalcFibo(n);
+
+            Console.WriteLine(result);
+        }
+
+        private static long CalcFibo(int n)
+        {
+            if (cache.ContainsKey(n))
+            {
+                return cache[n];
+            }
+
+            if (n < 2)
+            {
+                return n;
+            }            
+
+            var result = CalcFibo(n - 1) + CalcFibo(n - 2);
+
+            cache[n] = result;
+
+            return result;
+        }
+    }
+}
