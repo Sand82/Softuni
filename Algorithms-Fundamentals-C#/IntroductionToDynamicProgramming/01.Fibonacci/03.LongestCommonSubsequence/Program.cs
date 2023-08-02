533f7e4fb2cd9826f8ac8595df96720bf1449b8e
Sand82 <sandoki@abv.bg>
2023-08-02 21:19:33 +0300

    added new exercise

---
+++ Algorithms-Fundamentals-C#/IntroductionToDynamicProgramming/01.Fibonacci/03.LongestCommonSubsequence/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace LongestCommonSubsequence
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var firstStr = Console.ReadLine();
+            var secondStr = Console.ReadLine();
+
+            var matrix = new int[firstStr.Length + 1, secondStr.Length + 1];
+
+            for (int row = 1; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 1; col < matrix.GetLength(1); col++)
+                {
+
+                    if (firstStr[row - 1] == secondStr[col - 1])
+                    {
+                        matrix[row, col] = matrix[row - 1, col - 1] + 1;
+                    }
+                    else
+                    {
+                        matrix[row, col] = Math.Max(matrix[row, col - 1], matrix[row - 1, col]);
+                    }
+                }
+            }
+
+            Console.WriteLine(matrix[firstStr.Length, secondStr.Length]);
+        }
+    }
+}
