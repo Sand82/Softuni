c2b581d54077e2ea504809286f58ede926b34b45
Sand82 <sandoki@abv.bg>
2023-08-11 22:25:24 +0300

    added new exercise

---
+++ Algorithms-Fundamentals-C#/IntroductionToDynamicProgramming-Exercise/BinomialCoefficients/BinomialCoefficients/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace BinomialCoefficients
+{
+    public class Program
+    {
+        private static Dictionary<string, long> memo;
+        public static void Main(string[] args)
+        {
+            var row = int.Parse(Console.ReadLine());
+            var col = int.Parse(Console.ReadLine());
+
+            memo = new Dictionary<string, long>();
+
+            Console.WriteLine(GetBinom(row, col));
+        }
+
+        private static long GetBinom(int row, int col)
+        {
+            if (col == 0 || row == col)
+            {
+                return 1;
+            }
+
+            var key = $"{row}-{col}";
+
+            if (memo.ContainsKey(key))
+            {
+                return memo[key];
+            }
+
+            var result = GetBinom(row - 1, col - 1) + GetBinom(row - 1, col);
+
+            memo[key] = result;
+
+            return result;
+        }
+    }
+}
