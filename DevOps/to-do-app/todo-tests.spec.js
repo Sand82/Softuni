2757e64725aa1b0c80b43b34f7f3212c58773e1f
Sand82 <sandoki@abv.bg>
2023-11-11 21:53:04 +0200

    added new tests

--- DevOps/to-do-app/todo-tests.spec.js
+++ DevOps/to-do-app/todo-tests.spec.js
@@ -9,3 +9,39 @@ test('user can add a task', async ({page}) => {
 
     expect(taskText).toContain('Test Task');
 });
+
+test('user can delete a task', async ({page}) => {
+    await page.goto('http://127.0.0.1:5500/index.html');
+    await page.fill('#task-input', 'Test Task');
+    await page.click('#add-task');
+
+    await page.click('.task .delete-task');
+
+    const tasks = await page.$$eval('.task', 
+        tasks => tasks.map(task => task.textContent));
+
+    expect(tasks).not.toContain('Test Task');
+});
+
+test('user can delete a task as complete', async ({page}) => {
+    await page.goto('http://127.0.0.1:5500/index.html');
+    await page.fill('#task-input', 'Test Task');
+    await page.click('#add-task');
+
+    await page.click('.task .task-complete');
+
+    const complateTasks = await page.$('.task.completed');
+    expect(complateTasks).not.toContain('Test Task');
+});
+
+test('user can filter tasks', async ({page}) => {
+    await page.goto('http://127.0.0.1:5500/index.html');
+    await page.fill('#task-input', 'Test Task');
+    await page.click('#add-task');
+
+    await page.click('.task .task-complete');
+    await page.selectOption('#filter', 'Completed')
+
+    const incompletTasks = await page.$('.task:not(.completed)');
+    expect(incompletTasks).toBeNull();
+});
