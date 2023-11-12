b15c02a57b00ccf0ea4d828cf6ba1d6102a85f12
Sand82 <sandoki@abv.bg>
2023-11-12 18:58:36 +0200

    added new exercise

---
+++ DevOps/Automated-Testing-Resources/Library-Catalog/tests/ui.test.js
@@ -0,0 +1,11 @@
+const {test, expect} = require('@playwright/test');
+
+test('Veryfy "All Books" lonk is visible', async ({page}) => {
+    await page.goto('http://localhost:3000/');
+    await page.waitForSelector('nav.navbar');
+
+    const allBooksLink = await page.$('a[href="/catalog"]');
+    const isLinkVisible = await allBooksLink.isVisible();
+
+    expect(isLinkVisible).toBe(true);
+})
