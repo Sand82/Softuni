4e2386ff60592e14d31d2bbf4456afbbda82fc17
Sand82 <sandoki@abv.bg>
2023-11-12 19:16:08 +0200

    added new tests

--- DevOps/Automated-Testing-Resources/Library-Catalog/tests/ui.test.js
+++ DevOps/Automated-Testing-Resources/Library-Catalog/tests/ui.test.js
@@ -29,3 +29,30 @@ test('Veryfy "Register" button is visible', async ({page}) => {
 
     expect(isRegisterVisible).toBe(true);
 });
+
+test('Veryfy "All Books" link is visible after user login', async ({page}) => {
+    await userLogin(page);   
+
+    const allBooksLink = await page.$('a[href="/catalog"]');
+    const isLinkVisible = await allBooksLink.isVisible();
+
+    expect(isLinkVisible).toBe(true);
+});
+
+test.only('Veryfy "Add Books" button is visible', async ({page}) => {
+    await userLogin(page);
+    await page.waitForSelector('#user');
+
+    const addBookLink = await page.$('a[href="/create"]');
+    const isAddBookVisible = await addBookLink.isVisible();
+
+    expect(isAddBookVisible).toBe(true);
+});
+
+
+async function userLogin(page) {
+    await page.goto('http://localhost:3000/login');
+    await page.fill('input[name="email"]', 'peter@abv.bg');
+    await page.fill('input[name="password"]', '123456');
+    await page.click('input[type="submit"]');
+} 
