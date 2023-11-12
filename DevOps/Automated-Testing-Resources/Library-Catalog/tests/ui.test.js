06a16868f0ece1c45b980057d120fa63aca26398
Sand82 <sandoki@abv.bg>
2023-11-12 21:29:14 +0200

    added new exercise

--- DevOps/Automated-Testing-Resources/Library-Catalog/tests/ui.test.js
+++ DevOps/Automated-Testing-Resources/Library-Catalog/tests/ui.test.js
@@ -3,7 +3,7 @@ const {test, expect} = require('@playwright/test');
 const adminEmail = 'peter@abv.bg';
 const adminPassword = '123456';
 
-const userEmail = 'newuser@abv.bg'
+const userEmail = randomNumber() +'newuser@abv.bg' 
 const userPassword = '111111'
 const userRepeadPassword = '111111'
 
@@ -126,12 +126,28 @@ test('Login with empty adminPassword should return alert', async ({page}) => {
     expect(page.url()).toBe('http://localhost:3000/login');
 });
 
-test.only('Register with valid credentials', async ({page}) => {
+test('Register with valid credentials', async ({page}) => {
+    
     await userRegister(page, userEmail, userPassword, userRepeadPassword);    
 
     await page.$('a[href="/catalog"]');    
 
     expect(page.url()).toBe('http://localhost:3000/catalog');
+});
+
+test('Register with empty fields should return alert', async ({page}) => {
+    
+    await userRegister(page, " ", " ", " ");
+    
+    page.on('dalog', async dialog => {
+        expect(dialog.type()).toContain('alert');
+        expect(dialog.message()).toContain('All fields are required!');
+        await dialog.accept();   
+    })   
+
+    await page.$('a[href="/register"]');    
+
+    expect(page.url()).toBe('http://localhost:3000/register');
 });
 
 async function adminLogin(page, adminEmail, adminPassword) {
@@ -148,3 +164,8 @@ async function userRegister(page, userEmail, userPassword, repeatPassword) {
     await page.fill('input[name="confirm-pass"]', repeatPassword);
     await page.click('input[type="submit"]');
 } 
+
+function randomNumber() {
+    let number = Math.floor(Math.random() * 10000000000);
+    return number;    
+}
