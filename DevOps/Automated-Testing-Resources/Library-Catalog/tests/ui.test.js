087ccb7c40aa728057bb1cbde2c2d336fcdaf7f4
Sand82 <sandoki@abv.bg>
2023-11-12 19:01:44 +0200

    added new tests

--- DevOps/Automated-Testing-Resources/Library-Catalog/tests/ui.test.js
+++ DevOps/Automated-Testing-Resources/Library-Catalog/tests/ui.test.js
@@ -1,6 +1,6 @@
 const {test, expect} = require('@playwright/test');
 
-test('Veryfy "All Books" lonk is visible', async ({page}) => {
+test('Veryfy "All Books" link is visible', async ({page}) => {
     await page.goto('http://localhost:3000/');
     await page.waitForSelector('nav.navbar');
 
@@ -8,4 +8,24 @@ test('Veryfy "All Books" lonk is visible', async ({page}) => {
     const isLinkVisible = await allBooksLink.isVisible();
 
     expect(isLinkVisible).toBe(true);
-})
+});
+
+test('Veryfy "Login" button is visible', async ({page}) => {
+    await page.goto('http://localhost:3000/');
+    await page.waitForSelector('nav.navbar');
+
+    const loginLink = await page.$('a[href="/login"]');
+    const isLoginVisible = await loginLink.isVisible();
+
+    expect(isLoginVisible).toBe(true);
+});
+
+test('Veryfy "Register" button is visible', async ({page}) => {
+    await page.goto('http://localhost:3000/');
+    await page.waitForSelector('nav.navbar');
+
+    const registerLink = await page.$('a[href="/register"]');
+    const isRegisterVisible = await registerLink.isVisible();
+
+    expect(isRegisterVisible).toBe(true);
+});
