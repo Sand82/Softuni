e7a9fd0e60aad0c398d1604d4ba3aaac48cae1ea
Sand82 <sandoki@abv.bg>
2023-11-14 14:45:38 +0200

    added new tests

--- DevOps/AutomatedTesting-Exercise/Books/api.test.js
+++ DevOps/AutomatedTesting-Exercise/Books/api.test.js
@@ -47,4 +47,76 @@ describe('Book API', () => {
             });
 
     });
+
+    it('should GET Test Book', (done) => {
+        chai.request(server)    
+            .get(`/books/${bookId}`)
+            .end((err, res) => {
+                if(err){
+                    done();
+                }
+                expect(res).to.have.status(200);
+                expect(res.body).to.be.a(bookObject);
+                expect(res.body.id).to.be.equal(id);            
+                expect(res.body.author).to.be.equal(author);
+                expect(res.body.title).to.be.equal(title);
+                done();
+            });
+
+    });
+
+    it('should Update Test Book', (done) => {
+
+        let updatedBookAouthor = "Updated author";
+        let updatedBooktitle = "Updated book";
+        let updatedBook = {id : id, title: updatedBooktitle, author: updatedBookAouthor};
+
+        chai.request(server)    
+            .put(`/books/${bookId}`)
+            .send(updatedBook)
+            .end((err, res) => {
+                if(err){
+                    done();
+                }
+                expect(res).to.have.status(200);
+                expect(res.body).to.be.a(bookObject);
+                expect(res.body.id).to.be.equal(id);            
+                expect(res.body.author).to.be.equal(updatedBookAouthor);
+                expect(res.body.title).to.be.equal(updatedBooktitle);
+                done();
+            });
+
+    });
+
+    it('should GET, PUT or DELETE return 404 for non-existing book', (done) => {
+        chai.request(server)    
+            .get(`/books/400`)
+            .end((err, res) => {
+                if(err){
+                    done();
+                }
+                expect(res).to.have.status(404); 
+            });
+
+        chai.request(server)    
+            .put(`/books/400`)
+            .send({id: "400", title: "Non existing title", author: "No exsisting author"})
+            .end((err, res) => {
+                if(err){
+                    done();
+                }
+                expect(res).to.have.status(404); 
+            });
+
+        chai.request(server)    
+            .delete(`/books/400`)
+            .end((err, res) => {
+                if(err){
+                    done();
+                }
+                expect(res).to.have.status(404);                
+                done();
+            });
+
+    });
 })
