1366bba83458f8a75e0179044e018e4154f64234
Sand82 <sandoki@abv.bg>
2024-05-20 18:37:00 +0300

    added new exercise

---
+++ HackerRank/DiagonalDifference/DiagonalDifference/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{   
+
+    public static int DiagonalDifference(List<List<int>> arr)
+    {
+        var sumPrimaryDiagonal = 0;
+        var sumSecondaryDiagonal = 0;
+
+        var counterSecondary = arr.Count - 1;
+
+        for (int i = 0; i < arr.Count; i++)
+        {
+            var currPrimaryNum = arr[i][i];
+            var currSecondaryNum = arr[i][counterSecondary];
+
+            sumPrimaryDiagonal += currPrimaryNum;
+            sumSecondaryDiagonal += currSecondaryNum;
+
+            counterSecondary--;
+        }
+
+        var result = Math.Abs(sumPrimaryDiagonal - sumSecondaryDiagonal);
+
+        return result;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<List<int>> arr = new List<List<int>>();
+
+        for (int i = 0; i < n; i++)
+        {
+            arr.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList());
+        }
+
+        int result = Result.DiagonalDifference(arr);
+
+        Console.WriteLine(result);       
+    }
+}
