e498adfa5a8c78f8476945373a97bba37dce7e95
Sand82 <sandoki@abv.bg>
2024-05-22 18:53:07 +0300

    added new exercise

---
+++ HackerRank/MarsExploration/MarsExploration/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{    
+    public static int MarsExploration(string s)
+    {
+        var count = 0;
+
+        for (int i = 0; i < s.Length; i+=3)
+        {
+            var firstS = s[i];
+            var firstO = s[i+1];
+            var secondS = s[i+2];
+
+            if (firstS != 'S')
+            {
+                count++;
+            }
+
+            if (firstO != 'O')
+            {
+                count++;
+            }
+
+            if (secondS != 'S')
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {      
+        string s = Console.ReadLine();
+
+        int result = Result.MarsExploration(s);
+
+        Console.WriteLine(result);        
+    }
+}
