ad76dff10ea07ee73e65625ac2db79e9023c7a89
Sand82 <sandoki@abv.bg>
2024-05-24 17:10:22 +0300

    added new exercise

---
+++ HackerRank/FindMedian/FindMedian/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+
+
+class Result
+{
+
+    /*
+     * Complete the 'findMedian' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int FindMedian(List<int> arr)
+    {
+        var sorted = arr.OrderBy(x => x).ToList();
+
+        var element = sorted[sorted.Count / 2] ;
+
+        return sorted[element];
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        int result = Result.FindMedian(arr);
+
+        Console.WriteLine(result);
+        
+    }
+}
