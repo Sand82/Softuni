753560494ab19439f6bab47d20560d381fa9daf9
Sand82 <sandoki@abv.bg>
2024-05-26 22:30:41 +0300

    added new exercise

---
+++ XORStrings3/XORStrings3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Solution
+{
+    class Solution
+    {
+        public static void Main(string[] args)
+        {
+            var s = Console.ReadLine();
+
+            var t = Console.ReadLine();
+
+            var result = strings_xor(s, t);
+
+            Console.WriteLine(result);
+        }        
+
+        public static string strings_xor(string s, string t)
+        {           
+
+            var result = string.Empty;
+
+            for (int i = 0; i < s.Count(); i++)
+            {
+                var firstSymbol = s[i];
+                var secondSymbol = t[i];
+
+                if (firstSymbol == secondSymbol)
+                {
+                    result += '0';
+                }
+                else
+                {
+                    result += '1';
+                }
+            }
+
+            return result;
+        }
+    }
+}
