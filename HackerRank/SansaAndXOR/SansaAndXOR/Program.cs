14b27092b2dffe85fa1037a9c8460c977a229915
Sand82 <sandoki@abv.bg>
2024-06-07 14:18:39 +0300

    added new exercise

---
+++ HackerRank/SansaAndXOR/SansaAndXOR/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'sansaXor' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int sansaXor(List<int> arr)
+    {
+        int output = 0;
+
+        if (arr.Count % 2 == 0)
+            return output;
+
+        for (int i = 0; i < arr.Count; i += 2)
+            output ^= arr[i];
+
+        return output;
+    }
+}
+
+
+
+class Solution
+{
+    public static void Main(string[] args)
+    {        
+        int t = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int tItr = 0; tItr < t; tItr++)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+            int result = Result.sansaXor(arr);
+
+            Console.WriteLine(result);
+        }
+    }
+}
