db82997f6fa2be119fc44dab3a69188dbdc150ae
Sand82 <sandoki@abv.bg>
2024-07-21 00:03:24 +0300

    remove unwanted folder

---
+++ HackerRank/MisèreNim/MisèreNim/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{    
+    public static string MisereNim(List<int> s)
+    {
+
+
+        return null;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {  
+        int t = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int tItr = 0; tItr < t; tItr++)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> s = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(sTemp => Convert.ToInt32(sTemp)).ToList();
+
+            string result = Result.MisereNim(s);
+
+            Console.WriteLine(result);
+        }
+    }
+}
